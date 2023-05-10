bfe292d1fa91ea4d48c56f2ac3aa29742e8fa416
jyang-214 <jyang214@my.bcit.ca>
2023-05-09 20:25:55 -0700

    Modified Header

--- assignment-tracker/src/components/Header/index.tsx
+++ assignment-tracker/src/components/Header/index.tsx
@@ -1,18 +1,37 @@
+/** @format */
+
 import styles from "./header.module.css";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { uppercase } from "../../helpers/stringHelpers";
+import { useState } from "react";
 
 export function Header() {
-  return (
-    <header className={styles.header}>
-      {/* This is simply to show you how to use helper functions */}
-      <h1>{uppercase("bcit")} Assignment Tracker</h1>
-      <form className={styles.newAssignmentForm}>
-        <input placeholder="Add a new assignment" type="text" />
-        <button>
-          Create <AiOutlinePlusCircle size={20} />
-        </button>
-      </form>
-    </header>
-  );
+	const [inputValue, setInputValue] = useState("");
+
+	function handleInputChange(event) {
+		setInputValue(event.target.value.trim());
+	}
+
+	function handleCreateClick() {}
+
+	return (
+		<header className={styles.header}>
+			{/* This is simply to show you how to use helper functions */}
+			<h1>{uppercase("bcit")} Assignment Tracker</h1>
+			<form className={styles.newAssignmentForm}>
+				<input
+					placeholder="Add a new assignment"
+					type="text"
+					value={inputValue}
+					onChange={handleInputChange}
+				/>
+				<button
+					onClick={handleCreateClick}
+					disabled={inputValue === "" || /^\s*$/.test(inputValue)}
+				>
+					Create <AiOutlinePlusCircle size={20} />
+				</button>
+			</form>
+		</header>
+	);
 }
