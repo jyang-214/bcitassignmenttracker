bcc21f8eade323a5ccdf96a4caff840e67f697cc
jyang-214 <jyang214@my.bcit.ca>
2023-05-16 18:55:31 -0700

    In-Class assignment tracker

---
+++ provided-assignment-tracker/assignment-tracker/src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import { Assignments } from "./components/Assignments";
+
+function App() {
+	const [];
+	return (
+		<>
+			<Header />
+			<Assignments />
+		</>
+	);
+}
+
+export default App;
---
+++ provided-assignment-tracker/assignment-tracker/src/database.ts
@@ -0,0 +1,3 @@
+import IAssignment from './interfaces/IAssignment';
+
+export const assignmentData: IAssignment[] = [{title: "React Assignment Part 2", completed: false}];
